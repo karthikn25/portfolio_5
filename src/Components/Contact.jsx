0eaa712c59f593a557248c7a6b241ca976877237
karthikn25 <mkarthikeyan179@gmail.com>
2024-12-16 09:50:54 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,37 @@
+import Base from "../Base/Base"
+
 1
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <Base>
+    <div className="container">
+    <div className="row">
+    <div className="col">
+    <h2><span className="color">C</span>ontact <span className="color">M</span>e</h2>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col">
+    <p>Ready to collaborate? Reach out for projects, opportunities, or just to connect.Let's create impactful digital experiences together.</p>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col-12 col-sm-6">
+    <div className="contact">
+    <div className="contact-detail">
+    <div className="">
+    <i className='bx bx-envelope' ></i>
+    <h5>Have a question?</h5>
+    </div>
+    
+    </div>
+    </div>
+    
+    </div>
+    <div className="col-12 col-sm-6">
+    </div>
+    </div>
+    </div>
+    </Base>
   )
 }
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,136 @@
+import Base from "../Base/Base";
+import work1 from "../images/Kings_cafe_dashboard.png";
+import work2 from "../images/cafe_qr.png";
+import work3 from "../images/E-shipin.png";
+import work4 from "../images/JobPortal.png";
+import work5 from "../images/Private_chat_app.png";
+import work6 from "../images/instamart.png";
+import work7 from "../images/crud.png";
 
 export default function Project() {
   return (
-    <div>Project</div>
-  )
+    <Base>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div
+              className="project-box-container"
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "20px",
+                textAlign: "center",
+              }}
+            >
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work1}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work2}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work3}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work4}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work5}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work6}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="project-box">
+                <div className="project-box-content">
+                  <div className="project-img">
+                    <img
+                      src={work7}
+                      alt="projects"
+                      style={{ height: "200px", width: "200px" }}
+                    />
+                  </div>
+                  <div className="project-btn">
+                    <i className="bx bxl-github"></i>
+                    <i className="bx bx-globe"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Base>
+  );
 }
