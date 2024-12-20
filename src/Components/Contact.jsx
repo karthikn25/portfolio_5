eec6946e5a67d04a3e8443b6f27f42b20112494d
karthikn25 <mkarthikeyan179@gmail.com>
2024-12-20 09:43:43 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,39 +1,54 @@
-import Base from "../Base/Base"
-
+import Base from "../Base/Base";
 
 export default function Contact() {
   return (
     <Base>
-      <div className="">
-        <div className="row">
-          <div className="col">
-            <h2><span className="color">C</span>ontact <span className="color">M</span>e</h2>
+      <div className="contact-container">
+        <div className="row contact-head">
+          <div className="col contact">
+            <h2>
+              <span className="color">C</span>ontact{" "}
+              <span className="color">M</span>e
+            </h2>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <p>Ready to collaborate? Reach out for projects, opportunities, or just to connect.Let's create impactful digital experiences together.</p>
+        <div className="row contact-head">
+          <div className="col contact">
+            <p>
+              Ready to collaborate? Reach out for projects, opportunities, or
+              just to connect.Let's create impactful digital experiences
+              together.
+            </p>
           </div>
         </div>
-        <div className="row">
+        <div className="row contact-second-data">
           <div className="col-12 col-sm-6">
             <div className="contact">
               <div className="contact-detail">
-                <div className="">
-                  <i className='bx bx-envelope' ></i>
-                  <h5>Have a question?</h5>
-                  <p>I am here to help you</p>
-                  <h6 className="color">Email Me At mkarthikeyan@gmail.com</h6>
+                <div className="contact-detail-head">
+                  <div className="contact-detail-first-row">
+                    <i className="bx bx-envelope"></i>
+                    <div className="contact-detail-second-row">
+                      <h5>Have a question?</h5>
+                      <p>I am here to help you</p>
+                      <h6 className="color">
+                        Email Me At mkarthikeyan@gmail.com
+                      </h6>
+                    </div>
+                  </div>
                 </div>
-                <div className="">
-                  <i className='bx bx-map'></i>
-                  <h5>Current Location</h5>
-                  <p>Bangalore,India</p>
-                  <h6 className="color">Serving Clients Worldwide</h6>
+                <div className="contact-detail-head">
+                  <div className="contact-detail-first-row">
+                    <i className="bx bx-map"></i>
+                    <div className="contact-detail-second-row">
+                      <h5>Current Location</h5>
+                      <p>Bangalore,India</p>
+                      <h6 className="color">Serving Clients Worldwide</h6>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-
           </div>
           <div className="col-12 col-sm-6">
             <div className="contact-form">
@@ -52,14 +67,12 @@ export default function Contact() {
                 </div>
                 <div className="contact-btn">
                   <button>Send Message</button>
-
                 </div>
               </form>
-
             </div>
           </div>
         </div>
       </div>
     </Base>
-  )
+  );
 }
